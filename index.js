a29517c71c7a5793e89ab8b32f4a85d766bfed48
Satishkumar Sharma <satish@satishkumarsair.home>
2022-04-30 16:41:09 +0100

    adding more javascripts

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function square(number) {
 console.log(square(2));
 let x = 1;
 let y = 3;
+// Arithmatic operator
 console.log(x + y);
 console.log(x - y);
 console.log(x * y);
@@ -46,3 +47,68 @@ console.log(x !== 1);
 // Lose Equality
 console.log(x == 1);
 console.log(1 == '1');
+// Ternary Operator
+let points = 110;
+let type = points > 100 ? 'gold' : 'silver';
+console.log(type);
+// Logical Operator
+console.log(true && false);
+console.log(true || false);
+let isRefused = true;
+const job = !isRefused;
+console.log(job);
+// Falsy false undefined null 0 NaN
+let userColor = 'red';
+let defaultColor = 'blue';
+const currentColor = userColor || defaultColor;
+console.log(currentColor);
+// Bitwise
+// Read, Write and Execute
+// 00000100
+// 00000010
+// 00000001
+const readPermission = 4
+const writePermission = 2;
+const executePermission = 1;
+let myPermission = 0;
+myPermission = myPermission | readPermission | writePermission
+let message = (myPermission & readPermission) ? 'yes' : 'no';
+console.log(message);
+let a = 'red';
+let b = 'blue';
+let c = a;
+a = b;
+b = c;
+console.log(a);
+console.log(b);
+let hour = 10;
+if (hour >= 6 && hour < 12) console.log('Good Morning!');
+else if (hour >= 12 && hour < 18) console.log('Good Afternoon');
+else console.log('Good Evening!');
+let user = 'guest';
+switch (user) {
+    case 'guest':
+        console.log('Guest User');
+        break;
+    case 'moderator':
+        console.log('Moderator User');
+        break;
+    default:
+        console.log('Unknown User');
+}
+for (let i = 0; i <= 5; i++) {
+    if (i % 2 !== 0) console.log(i);
+}
+for (let i = 5; i > 5; i--) {
+    if (i % 2 !== 0) console.log(i);
+}
+let i = 0;
+while (i <= 5) {
+    if (i % 2 !== 0) console.log(i);
+    i++;
+}
+let j = 9;
+do {
+    if (j % 2 !== 0) console.log(j);
+    j++;
+} while (j <= 5)
